f6192f221b106e753677d8ae8590859749139e05
Francisco Juarez <franjuarez2013@gmail.com>
2025-08-22 14:25:22 -0300

    Agrego conexion BD

--- Alfred2/Program.cs
+++ Alfred2/Program.cs
@@ -7,13 +7,25 @@ var builder = WebApplication.CreateBuilder(args);
 
 // EF Core (SQL Server)
 builder.Services.AddDbContext<AppDbContext>(opt =>
-    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
+    opt.UseNpgsql(Environment.GetEnvironmentVariable("DB_CONNECTION")));
 // Servicios propios
 builder.Services.AddHttpClient<OpenAIChatService>();
 builder.Services.AddHttpClient("whatsapp");
+
 builder.Services.AddControllers();
+//Integrar el frontend con CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFE",
+        p => p.WithOrigins("https://alfredbot.vercel.app")
+              .AllowAnyHeader()
+              .AllowAnyMethod());
+});
+var port = Environment.GetEnvironmentVariable("PORT") ?? "10000";
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
 
 var app = builder.Build();
+app.MapGet("/healthz", () => Results.Ok(new { status = "ok" }));
+app.UseCors("AllowFE");
 app.MapControllers();
 app.Run();
