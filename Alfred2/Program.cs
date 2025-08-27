d82bcf9979ffca8e25cbc1c8644fecf775dae9b0
Francisco Juarez <franjuarez2013@gmail.com>
2025-08-27 17:33:31 -0300

    Variable entorno DB

--- Alfred2/Program.cs
+++ Alfred2/Program.cs
@@ -1,13 +1,21 @@
 using Microsoft.EntityFrameworkCore;
 using Alfred2.DBContext;
 using Alfred2.OpenAIService;
-using Alfred2.Models;
+using Alfred2.Models;                   
 
 var builder = WebApplication.CreateBuilder(args);
 
-// EF Core (SQL Server)
 builder.Services.AddDbContext<AppDbContext>(opt =>
-    opt.UseNpgsql(Environment.GetEnvironmentVariable("DB_CONNECTION")));
+{
+    opt.UseNpgsql(Environment.GetEnvironmentVariable("DB_CONNECTION"));
+    opt.EnableDetailedErrors();
+    opt.EnableSensitiveDataLogging(); // SOLO para depurar
+});
+
+// logs de EF a consola
+builder.Logging.AddConsole();
+builder.Logging.AddFilter("Microsoft.EntityFrameworkCore.Database.Command", LogLevel.Information);
+builder.Logging.AddFilter("Microsoft.EntityFrameworkCore.Query", LogLevel.Debug);
 // Servicios propios
 builder.Services.AddHttpClient<OpenAIChatService>();
 builder.Services.AddHttpClient("whatsapp");
@@ -25,6 +33,12 @@ var port = Environment.GetEnvironmentVariable("PORT") ?? "10000";
 builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
 
 var app = builder.Build();
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    await db.Database.MigrateAsync(); // aplica migraciones pendientes
+    // opcional: var ok = await db.Database.CanConnectAsync(); Console.WriteLine($"DB connect: {ok}");
+}
 app.MapGet("/healthz", () => Results.Ok(new { status = "ok" }));
 app.UseCors("AllowFE");
 app.MapControllers();
